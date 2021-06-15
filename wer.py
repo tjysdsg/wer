f62ec31d7cfebd368ea81f4a5fc64417f218d3b6
tjysdsg <tjy1018543509@163.com>
2021-06-15 11:28:34 +0800

    Save alignment results as table

--- wer.py
+++ wer.py
@@ -13,23 +13,13 @@ def get_args():
         description='Calculate ASE correlation between predicted scores and annotated scores')
     parser.add_argument('hyp', metavar='HYP', type=str, help='Hypothesis file')
     parser.add_argument('ref', metavar='REF', type=str, help='Reference file')
-    parser.add_argument('--output-path', type=str, default='tmp/results.txt', help='Path to write results')
+    parser.add_argument('--output-path', type=str, default='data/results.txt', help='Path to write results')
     args = parser.parse_args()
     return args
 
 
-def get_wer_align(hyps: Dict[str, List[str]], refs: Dict[str, List[str]]) -> Dict:
-    ref_list = []
-    hyp_list = []
-    utts = []
-    for utt, h in hyps.items():
-        utts.append(utt)
-        hyp_list.append(h)
-        ref_list.append(refs[utt])
-
-    details = wer_details_for_batch(utts, ref_list, hyp_list, compute_alignments=True)
-
-    # get utt -> wer alignments
+def get_wer_align(details) -> Dict:
+    # {utt -> wer alignments}
     wer_align = {}
     for d in details:
         wer_align[d['key']] = d['alignment']
@@ -37,43 +27,29 @@ def get_wer_align(hyps: Dict[str, List[str]], refs: Dict[str, List[str]]) -> Dic
     return wer_align
 
 
-def get_result_str(wer_align: List, hyp: List[str], ref: List[str], pred: List[float], label: List[float]) -> str:
-    pred = [str(int(score)) for score in pred]
-    label = [str(int(score)) for score in label]
-
+def get_result_str(wer_align: List, hyp: List[str], ref: List[str]) -> str:
     n = len(wer_align)
-    lines = ['' for _ in range(4)]
-    indices = [0 for _ in range(3)]
+    lines = ['' for _ in range(2)]
+    indices = [0 for _ in range(2)]
     for i in range(n):
         err = wer_align[i][0]
         if err == 'S' or err == '=':
             lines[0] += '\t' + hyp[indices[0]]
             lines[1] += '\t' + ref[indices[1]]
-            lines[2] += '\t' + pred[indices[2]]
-            lines[3] += '\t' + label[indices[2]]
             indices[0] += 1
             indices[1] += 1
-            indices[2] += 1
         elif err == 'I':
             lines[0] += '\t' + hyp[indices[0]]
             lines[1] += '\t '
-            lines[2] += '\t '
-            lines[3] += '\t '
             indices[0] += 1
         elif err == 'D':
             lines[0] += '\t '
             lines[1] += '\t' + ref[indices[1]]
-            lines[2] += '\t' + pred[indices[2]]
-            lines[3] += '\t' + label[indices[2]]
             indices[1] += 1
-            indices[2] += 1
         else:
             assert False
 
-    return f'pred_phones:\t{lines[0]}\n' \
-           f'true_phones:\t{lines[1]}\n' \
-           f'pred_scores:\t{lines[2]}\n' \
-           f'true_scores:\t{lines[3]}\n'
+    return f'pred_phones:\t{lines[0]}\ntrue_phones:\t{lines[1]}\n'
 
 
 def main():
@@ -85,7 +61,7 @@ def main():
             tokens = line.strip('\n').split()
             utt = tokens[0]
             phones = tokens[1:]
-            hyp[utt] = phones
+            hyp[utt] = clean_phones(phones)
 
     ref = {}
     with open(args.ref, encoding='utf-8') as f:
@@ -94,7 +70,7 @@ def main():
             utt = tokens[0]
             phones = tokens[1:]
             if utt in hyp:
-                ref[utt] = phones
+                ref[utt] = clean_phones(phones)
 
     ref_list = []
     hyp_list = []
@@ -102,29 +78,21 @@ def main():
     for utt, h in hyp.items():
         if utt in ref:
             utts.append(utt)
-            hyp_list.append(clean_phones(h))
-            ref_list.append(clean_phones(ref[utt]))
+            hyp_list.append(h)
+            ref_list.append(ref[utt])
 
-    details = wer_details_for_batch(utts, ref_list, hyp_list, scoring_mode='present')
-
+    details = wer_details_for_batch(utts, ref_list, hyp_list, scoring_mode='present', compute_alignments=True)
     print(wer_summary(details))
 
-    # scores = predict_scores(utts, details)
-    # preds, wer_align = get_wer_align(hyps, refs)
-    # f = open(args.output_path, 'w')
-    # hyp_scores = []
-    # true_scores = []
-    # for utt, s in preds.items():
-    #     hyp_scores += s
-    #     if utt in labels:
-    #         true_scores += labels[utt]
-    #         error_type = wer_align[utt]
-    #         f.write(f'utt: {utt}\n')
-    #         f.write(get_result_str(error_type, hyps[utt], refs[utt], preds[utt], labels[utt]))
-    #     else:
-    #         print(f'WARNING: Cannot find annotated score for {utt}')
+    wer_align = get_wer_align(details)
+    f = open(args.output_path, 'w')
+    for utt, s in hyp.items():
+        if utt in ref:
+            error_type = wer_align[utt]
+            f.write(f'utt: {utt}\n')
+            f.write(get_result_str(error_type, hyp[utt], ref[utt]))
 
-    # f.close()
+    f.close()
 
 
 if __name__ == '__main__':
